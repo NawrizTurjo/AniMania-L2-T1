4e9bd797c32184983ed6b47ba749cfc4c528a527
NawrizTurjo <nawriz_ahmed03@hotmail.com>
2024-01-22 22:09:22 +0600

    enhance

--- client/src/pages/animeItem.js
+++ client/src/pages/animeItem.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import EditIcon from '@mui/icons-material/Edit';
 
 const AnimeListItem = ({
   title,
@@ -12,47 +17,56 @@ const AnimeListItem = ({
   id,
 }) => (
   <li className="list-group-item align-items-center">
-    <a href={`http://localhost:3001/anime/${id}`}>
-      {thumbnail && (
-        <img src={thumbnail} alt={`${title} Thumbnail`} className="thumbnail" />
-      )}
-    </a>
+    {thumbnail && (
+      <img src={thumbnail} alt={`${title} Thumbnail`} className="thumbnail" />
+    )}
+
     <h6>{title}</h6>
     <p>
       Num of episodes: {ep != null ? ep : "N/A"} | Type:{" "}
-      {anime_type != null ? anime_type : "N/A"} | {age_rating != null ? age_rating : "N/A"}
+      {anime_type != null ? anime_type : "N/A"} |{" "}
+      {age_rating != null ? age_rating : "N/A"}
     </p>
     <p>Demographic: {demo != null ? demo : "N/A"}</p>
     <p>
       Season: {season != null ? season : "N/A"} | Year:{" "}
       {yr != null ? yr : "N/A"}
     </p>
+    {/* <button className="btn btn-primary">Edit</button>
+    <button className="btn btn-primary">Delete</button> */}
+    <Stack direction="row" spacing={10}>
+    <a href={`http://localhost:3001/anime/${id}`}>
+      <Button variant="outlined" color="secondary" startIcon={<EditIcon />}>Edit</Button>
+    </a>
+      <Button  color="secondary" ><FavoriteIcon /></Button>
+      <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>Delete</Button>
+    </Stack>
   </li>
 );
 
-const AnimeItem =({currentanimes,loading})=>{
-    if(loading){
-        return <h2>Loading...</h2>
-    }
+const AnimeItem = ({ currentanimes, loading }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
 
-    return(
-        <ul className="list-group list-group-horizontal-md flex-row flex-wrap">
-              {currentanimes.map((anime, index) => (
-                <AnimeListItem
-                  key={index}
-                  title={anime.anime_name}
-                  ep={anime.number_of_episodes}
-                  anime_type={anime.anime_type}
-                  age_rating={anime.age_rating}
-                  demo={anime.demographic}
-                  season={anime.season}
-                  yr={anime.year}
-                  thumbnail={anime.title_screen}
-                  id={anime.anime_id}
-                />
-              ))}
-        </ul>
-    )
-}
+  return (
+    <ul className="list-group list-group-horizontal-md flex-row flex-wrap">
+      {currentanimes.map((anime, index) => (
+        <AnimeListItem
+          key={index}
+          title={anime.anime_name}
+          ep={anime.number_of_episodes}
+          anime_type={anime.anime_type}
+          age_rating={anime.age_rating}
+          demo={anime.demographic}
+          season={anime.season}
+          yr={anime.year}
+          thumbnail={anime.title_screen}
+          id={anime.anime_id}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default AnimeItem;
--- client/src/pages/genrePage.js
+++ client/src/pages/genrePage.js
@@ -10,7 +10,6 @@ import TableRow from "@mui/material/TableRow";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 import "./GenreAnimes.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -22,33 +21,39 @@ const columns = [
     format: (value) => value.toLocaleString("en-US"),
   },
   { id: "anime_id", label: "Id", minWidth: 100,format: (value) => value.toLocaleString("en-US"), },
-  //   { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
   {
     id: "number_of_episodes",
     label: "Total episodes",
     minWidth: 170,
-    align: "right",
+    align: "center",
     format: (value) => value.toFixed(0),
+  },
+  {
+    id: "anime_name",
+    label: "Title",
+    minWidth: 170,
+    align: "center",
+    format: (value) => value.toLocaleString("en-US"),
   },
   {
     id: "anime_type",
     label: "Type",
     minWidth: 170,
-    align: "right",
+    align: "center",
     format: (value) => value.toLocaleString("en-US"),
   },
   {
     id: "age_rating",
     label: "Age_rating",
     minWidth: 170,
-    align: "right",
+    align: "center",
     format: (value) => value.toFixed(2),
   },
   {
     id: "season",
     label: "Season",
     minWidth: 170,
-    align: "right",
+    align: "center",
     format: (value) => value.toLocaleString("en-US"),
   },
   {
@@ -67,32 +72,9 @@ const columns = [
   },
 ];
 
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
 export default function GenrePage({ id, name }) {
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const [loading, setLoading] = useState(true);
   const [animeList, setAnimeList] = useState([]);
@@ -127,7 +109,7 @@ export default function GenrePage({ id, name }) {
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              <TableCell align="center" colSpan={8}>
+              <TableCell align="center" colSpan={9}>
                 <a href={`http://localhost:3001/genre/${id}`}>
                   <h1>{name}</h1>
                 </a>
@@ -159,9 +141,12 @@ export default function GenrePage({ id, name }) {
                         console.log(`${column.id}:`, value);
                       return (
                         <TableCell key={column.id} align={column.align}>
-                          {column.id === 'title_screen' ? (
+                          {column.id === 'title_screen' ? (<a
+                            
+                            href={`http://localhost:3001/anime/${anime.anime_id}`}
+                            className="list-group-item d-flex align-items-center">
                             <img src={value} alt="Title Screen" style={{ maxWidth: '50px', maxHeight: '75px' }} />
-                          ) : (
+                            </a>) : (
                             column.format && typeof value === 'number' ? column.format(value) : value
                           )}
                         </TableCell>
@@ -175,7 +160,7 @@ export default function GenrePage({ id, name }) {
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
+        rowsPerPageOptions={[5, 10, 25, 100,500]}
         component="div"
         count={animeList.length}
         rowsPerPage={rowsPerPage}
