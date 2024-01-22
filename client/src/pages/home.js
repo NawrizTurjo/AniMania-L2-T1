0f2dd0a7ccc42b3bf0523cec04ecc3bd33ace81e
Shams-Simanto <shams.hossain.simanto@gmail.com>
2024-01-23 05:00:10 +0600

    pagination2

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Pagination from "./pagination";
+import Pagination from "./pagination2";
 import AnimeItem from "./animeItem";
 //import { useParams } from 'react-router-dom';
 import { useLocation } from "react-router-dom";
