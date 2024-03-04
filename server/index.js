d464c06dddcfd777ebecdb9e9a5f0854232df615
NawrizTurjo <nawriz_ahmed03@hotmail.com>
2024-03-05 02:11:55 +0600

    toast_new

--- server/index.js
+++ server/index.js
@@ -147,7 +147,7 @@ app.post("/AdvancedSearch", async (req, res) => {
       WHERE 1 = 1`;
 
     if (searchString) {
-      const editDistanceThreshold = 2;
+      const editDistanceThreshold = 3;
 
       query += ` AND EXISTS (
         SELECT 1
@@ -388,7 +388,7 @@ app.post("/home", async (req, res) => {
         GROUP BY 
             a.anime_id,ta.user_id,ta.status
         ORDER BY 
-            a.mal_score DESC
+            a.year desc
             )
 
       `,
@@ -1383,43 +1383,50 @@ app.post("/searchAnime", async (req, res) => {
     const { userEmail, searchTerm } = req.body;
     console.log(userEmail, searchTerm);
 
+    const editDistanceThreshold = 3;
+
     const allAnimes = await pool.query(
       `
-      with T AS(
-        SELECT DISTINCT (anime_id),user_id,status
+      WITH T AS (
+        SELECT DISTINCT (anime_id), user_id, status
         FROM users_anime_list ua
-        where user_id = (
-          SELECT EMAIL_TO_ID($1) as "id"
+        WHERE user_id = (
+            SELECT EMAIL_TO_ID($1) AS "id"
         )
-        )
-        
-        (
-        SELECT
-            a.*
-            ,
-            string_agg(DISTINCT g.genre_name, ',') AS genres,
-            ta.user_id AS user_id
-            ,
-            CASE WHEN ta.user_id IS NOT NULL THEN true ELSE false END AS is_favorite,
-						CASE WHEN ta.user_id IS NOT NULL THEN ta.status ELSE NULL END AS status
-        FROM 
-            anime a
-        LEFT JOIN 
-            genre_anime_relationship ga ON ga.anime_id = a.anime_id
-        LEFT JOIN 
-            genres g ON g.genre_id = ga.genre_id
-        LEFT JOIN 
-            T ta on ta.anime_id = a.anime_id
-        WHERE a.anime_name ILIKE $2
-        GROUP BY 
-            a.anime_id,ta.user_id,ta.status
-        ORDER BY 
-            a.mal_score DESC
-            )
+    ),
+    ED AS (
+        SELECT a.anime_id
+        FROM anime a
+        CROSS JOIN generate_series(1, LENGTH(a.anime_name)) AS i
+        WHERE LEVENSHTEIN(UPPER(SUBSTRING(a.anime_name FROM i FOR LENGTH($2))), UPPER($2)) <= ${editDistanceThreshold}
+    )
+    SELECT
+        a.*,
+        string_agg(DISTINCT g.genre_name, ',') AS genres,
+        ta.user_id AS user_id,
+        CASE WHEN ta.user_id IS NOT NULL THEN true ELSE false END AS is_favorite,
+        CASE WHEN ta.user_id IS NOT NULL THEN ta.status ELSE NULL END AS status
+    FROM 
+        anime a
+    LEFT JOIN 
+        genre_anime_relationship ga ON ga.anime_id = a.anime_id
+    LEFT JOIN 
+        genres g ON g.genre_id = ga.genre_id
+    LEFT JOIN 
+        T ta on ta.anime_id = a.anime_id
+    WHERE a.anime_id IN (SELECT anime_id FROM ED)
+    GROUP BY 
+        a.anime_id, ta.user_id, ta.status
+    ORDER BY 
+        a.mal_score DESC
 
       `,
       [userEmail, `%${searchTerm}%`]
     );
+    /*
+    FROM generate_series(1, LENGTH(a.anime_name)) AS i
+        WHERE LEVENSHTEIN(UPPER(SUBSTRING(a.anime_name FROM i FOR LENGTH('${searchString}'))), UPPER('${searchString}')) <= ${editDistanceThreshold}
+    */
 
     res.header("Access-Control-Allow-Origin", "http://localhost:3001");
     // res.json();
@@ -2232,31 +2239,30 @@ app.post("/getNotifications", async (req, res) => {
   }
 });
 
-
 app.post("/addAnime", async (req, res) => {
   const {
-      anime_name,
-      title_screen,
-      total_episodes,
-      description,
+    anime_name,
+    title_screen,
+    total_episodes,
+    description,
 
-     // searchString: searchString,
-      season,
-      genre,
-      tag,
-      year,
-      ageRating,
-      //rating: rating,
-      type,
-      demographic,
-      source,
-      opening_soundtrack,
-      ending_soundtrack,
-      streaming_sites,
-      mal_score,
-      //episodeCount: episodes,
-      //characters: characters,
-      userEmail,
+    // searchString: searchString,
+    season,
+    genre,
+    tag,
+    year,
+    ageRating,
+    //rating: rating,
+    type,
+    demographic,
+    source,
+    opening_soundtrack,
+    ending_soundtrack,
+    streaming_sites,
+    mal_score,
+    //episodeCount: episodes,
+    //characters: characters,
+    userEmail,
   } = req.body;
   console.log(userEmail);
   try {
@@ -2267,71 +2273,93 @@ app.post("/addAnime", async (req, res) => {
     // );
     // console.log(ani_id);
     const yearIn = isNaN(parseInt(year)) ? 0 : parseInt(year);
-    const number_of_episodes_In = isNaN(parseInt(total_episodes)) ? 0 : parseInt(total_episodes);
+    const number_of_episodes_In = isNaN(parseInt(total_episodes))
+      ? 0
+      : parseInt(total_episodes);
     const mal_score_In = isNaN(parseInt(mal_score)) ? 0 : parseInt(mal_score);
 
-
     const newAnimeRes = await pool.query(
-      'INSERT INTO anime (anime_name, title_screen, number_of_episodes, description, year, age_rating, anime_type, demographic, anime_source, streaming_sites, mal_score, season) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING anime_id',
-      [anime_name, title_screen, number_of_episodes_In, description, yearIn, ageRating, type, demographic, source,  streaming_sites, mal_score_In,season]
-  );
-  const ani_id = newAnimeRes.rows[0].anime_id;
-  console.log(ani_id);
-  const newsOound = await pool.query(
-    `INSERT INTO sound_tracks (anime_id, title) VALUES($1, $2)`, [ani_id, opening_soundtrack]
-  );
-  const newsEound = await pool.query(
-    `INSERT INTO sound_tracks (anime_id, title) VALUES($1, $2)`, [ani_id, ending_soundtrack]
-  );
+      "INSERT INTO anime (anime_name, title_screen, number_of_episodes, description, year, age_rating, anime_type, demographic, anime_source, streaming_sites, mal_score, season) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING anime_id",
+      [
+        anime_name,
+        title_screen,
+        number_of_episodes_In,
+        description,
+        yearIn,
+        ageRating,
+        type,
+        demographic,
+        source,
+        streaming_sites,
+        mal_score_In,
+        season,
+      ]
+    );
+    const ani_id = newAnimeRes.rows[0].anime_id;
+    console.log(ani_id);
+    const newsOound = await pool.query(
+      `INSERT INTO sound_tracks (anime_id, title) VALUES($1, $2)`,
+      [ani_id, opening_soundtrack]
+    );
+    const newsEound = await pool.query(
+      `INSERT INTO sound_tracks (anime_id, title) VALUES($1, $2)`,
+      [ani_id, ending_soundtrack]
+    );
 
+    for (const genreName of genre.split(", ")) {
+      const genreResult = await pool.query(
+        `SELECT genre_id FROM genres WHERE genre_name=$1`,
+        [genreName]
+      );
+      // Extract the genre_id from the result
+      if (genreResult.rows.length > 0) {
+        const genreId = genreResult.rows[0].genre_id;
+        await pool.query(
+          "INSERT INTO genre_anime_relationship (anime_id, genre_id) VALUES ($1, $2)",
+          [ani_id, genreId]
+        );
+      } else {
+        // Handle the case where the genre does not exist, if necessary
+        console.log(`Genre not found: ${genreName}`);
+      }
+    }
 
-for (const genreName of genre.split(', ')) {
-  const genreResult = await pool.query(
-    `SELECT genre_id FROM genres WHERE genre_name=$1`, [genreName]
-  );
-  // Extract the genre_id from the result
-  if (genreResult.rows.length > 0) {
-    const genreId = genreResult.rows[0].genre_id;
-    await pool.query('INSERT INTO genre_anime_relationship (anime_id, genre_id) VALUES ($1, $2)', [ani_id, genreId]);
-  } else {
-    // Handle the case where the genre does not exist, if necessary
-    console.log(`Genre not found: ${genreName}`);
-  }
-}
+    for (const tagName of tag.split(", ")) {
+      const tagResult = await pool.query(
+        `SELECT tag_id FROM tags WHERE tag_name=$1`,
+        [tagName]
+      );
+      // Extract the tag_id from the result
+      if (tagResult.rows.length > 0) {
+        const tagId = tagResult.rows[0].tag_id;
+        await pool.query(
+          "INSERT INTO tag_id_table(anime_id, tag_id) VALUES ($1, $2)",
+          [ani_id, tagId]
+        );
+      } else {
+        // Handle the case where the tag does not exist, if necessary
+        console.log(`Tag not found: ${tagName}`);
+      }
+    }
 
-for (const tagName of tag.split(', ')) {
-  const tagResult = await pool.query(
-    `SELECT tag_id FROM tags WHERE tag_name=$1`, [tagName]
-  );
-  // Extract the tag_id from the result
-  if (tagResult.rows.length > 0) {
-    const tagId = tagResult.rows[0].tag_id;
-    await pool.query('INSERT INTO tag_id_table(anime_id, tag_id) VALUES ($1, $2)', [ani_id, tagId]);
-  } else {
-    // Handle the case where the tag does not exist, if necessary
-    console.log(`Tag not found: ${tagName}`);
-  }
-}
-
-const user_id = await pool.query(
-  `
+    const user_id = await pool.query(
+      `
   SELECT "id"
   FROM person
   WHERE email = $1
   `,
-  [userEmail]
-);
-const id = user_id.rows[0].id;
+      [userEmail]
+    );
+    const id = user_id.rows[0].id;
 
-const updateanimeadd = await pool.query(
-  `
+    const updateanimeadd = await pool.query(
+      `
   update moderator set added_series= added_series+1 where moderator_id=$1
   `,
-  [id]
-);
+      [id]
+    );
 
-res.json({ message: 'Anime added successfully!' });
-
+    res.json({ message: "Anime added successfully!" });
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ error: "Internal Server Error" });
